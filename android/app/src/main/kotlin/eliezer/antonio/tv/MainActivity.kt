4a290541f2922f39c1fc5ffae287c0adf0334418
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-12-22 21:54:09 +0100

    The Classic First Commit

---
+++ android/app/src/main/kotlin/eliezer/antonio/tv/MainActivity.kt
@@ -0,0 +1,5 @@
+package eliezer.antonio.tv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:tv/app/my_app.dart';
+
+void main() => MyApp();
